5835e856b9998f39833f11eb990577f767cf01d3
Kenneth-mijares <kenneth.mijares2003@gmail.com>
2024-10-25 10:54:35 +0800

    changes on landing page ui

--- lib/pages/landing page/landing_page.dart
+++ lib/pages/landing page/landing_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:capstone/pages/landing%20page/widgets/buttons/Upper%20limb/big_Button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
@@ -14,22 +15,75 @@ class LandingPage extends StatelessWidget {
         title: const Text('Exercises'),
         backgroundColor: const Color.fromARGB(255, 111, 128, 222),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Column(
             
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const SizedBox(height: 18),
+
+              //DISCOVER ARTICLES
+
+              Text(
+                'DISCOVER',
+                   style: GoogleFonts.kanit(
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color:  Color.fromARGB(255, 111, 128, 222), 
+                        ),
+                      ), 
+                
+              ),
+              const SizedBox(height: 12),
+
+              const BigButton(), // TODO: edit this one out eg replace
             
-              SizedBox(height: 18),
-              BigButton(),
+              const SizedBox(height: 20),
+
+
+              // RECOMMENDED WORK OUTS
+
+              Text(
+                'Recommended Workout',
+                   style: GoogleFonts.kanit(
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color:  Color.fromARGB(255, 111, 128, 222), 
+                        ),
+                      ), 
+                
+              ),
+              const SizedBox(height: 12),
+
+              const BigButton(), // TODO: edit this one out eg replace
+
+              const SizedBox(height: 20),
+
+              // AVAILABLE EXERCISES
+              
+              Text(
+                'Available Exercises',
+                   style: GoogleFonts.kanit(
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color:  Color.fromARGB(255, 111, 128, 222), 
+                        ),
+                      ), 
+                
+              ),
+              const SizedBox(height: 12),
+              const BigButton(),
            
-              SizedBox(height: 18),
+              const SizedBox(height: 18),
            
-              SizedBox(height: 18),
+              const SizedBox(height: 18),
           
-              SizedBox(height: 18),
+              const SizedBox(height: 18),
             ],
           ),
         ),
